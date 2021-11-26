9b73c082d1a3e4d8fe61a9a2fcb23d10f3500de9
David Saah <dasorange.hope@gmail.com>
2021-11-26 00:16:18 +0000

    add site is under construction message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+// import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
 function App() {
   return (
-    <div>
-      <h1>Welcome Code Review Shop</h1>
+    <div className='container text-center'>
+      <h1>Welcome To Code Review Shop</h1>
+      <h5 className='text-muted'>Together is Stronger</h5><br/>
+      <p>Site is currently under construction</p>
     </div>
   );
 }
