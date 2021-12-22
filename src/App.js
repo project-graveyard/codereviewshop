2e2ffb2c663bbf10702587e901c9e94399982e70
David Saah <dasorange.hope@gmail.com>
2021-12-22 15:22:35 +0000

    add bootstrap imports, and declare routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 // import bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import bootstrap icons
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from "./Pages/About/About";
+import Orders from "./Pages/Orders/Orders";
+import Comm from "./Pages/Community/Comm";
+import Services from "./Pages/Services/Services";
+
 
 
 function App() {
   return (
-    <div className='container text-center'>
-      <h1>Welcome To Code Review Shop</h1>
-      <h5 className='text-muted'>Together is Stronger</h5><br/>
-      <p>Site is currently under construction</p>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} exact/>
+        <Route path='/about' element={<About />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/community' element={<Comm />} />
+        <Route path='/services' element={<Services />} />
+      </Routes>
     </div>
   );
 }
