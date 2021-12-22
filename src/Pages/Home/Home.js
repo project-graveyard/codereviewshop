aba3afac6128f7ace48c168d474cad831a281b42
David Saah <dasorange.hope@gmail.com>
2021-12-22 17:51:35 +0000

    add image and welcome message to homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import "./Home.css";
+import homeImg from "../../Assets/images/homeImg.jpg";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div>
-            <h1>Homepage</h1>
+  return (
+    <div className="home">
+      <div className="home__welcome">
+        <img src={homeImg} alt="People reviewing code" className="home__img" />
+        <div className="home__text">
+          <h1>Welcome To CodeReviewShop</h1>
+          <p className="display-6">
+            CodeReviewShop is a place where you can find the best code review
+            services. We have a variety of services to choose from.
+          </p>
+          <Link to="/services">
+            <button className="btn btn-primary">Book Now</button>
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Home;
