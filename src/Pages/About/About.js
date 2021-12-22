5d41487ac217c91d4c2e3fdb31040b653b9a9620
David Saah <dasorange.hope@gmail.com>
2021-12-22 18:01:37 +0000

    update About page with demo info

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./About.css";
+import review from "../../Assets/images/review.jpg";
 
 function About() {
-    return (
-        <div>
-            <h1>About Us Page</h1>
-        </div>
-    )
+  return (
+    <div className="about">
+    <img src={review} alt="Person reviewing code" className="about__img" />
+      <div className="about__text">
+        <h1>About Us</h1>
+        <p className="display-6">
+          CodeReviewShop is a place where you can find the best code review
+          services. We have a variety of services to choose from.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
