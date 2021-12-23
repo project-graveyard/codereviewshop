97fb778f268d4f16712ec8c57b57824e5541cb47
David Saah <dasorange.hope@gmail.com>
2021-12-23 18:08:18 +0000

    add service list variable to hold service details and update service page

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,14 +1,60 @@
 import React from "react";
 import "./Services.css";
-import code from "../../Assets/images/services.jpg";
+
+const service_list = [
+  {
+    title: "Code review & feedback",
+    price: "Free",
+    details: "Details",
+  },
+  {
+    title: "One-on-one tutoring",
+    price: "15",
+    details: "Details",
+  },
+  {
+    title: "Code walkthrough & assistance",
+    price: "15",
+    details: "Details",
+  },
+  {
+    title: "IT support & consultation",
+    price: "*",
+    details: "Details",
+  },
+];
 
 function Services() {
   return (
-    <div className="services">
-      <img src={code} alt="Code on the screen" className="services__img" />
-      <div className="services__text">
-        <h1>Services Page</h1>
-        <p className="display-6">List of services will be here.</p>
+    <div className="container">
+      <div className="text-center p-3 pb-md-4 mx-auto">
+        <h1 className="display-4 fw-normal">Services & Pricing</h1>
+        <p className="fs-5 text-muted">Choose a service and a plan that works best for you</p>
+      </div>
+      <div className="services__data">
+        <div className="row row-cols-1 row-cols-md-4 mb-3 text-center">
+          {service_list.map((service, index) => (
+            <div className="col">
+              <div className="card mb-4 rounded-3 shadow-sm border-primary h-100">
+                <div className="card-header py-3 bg-primary text-white border-primary">
+                  <h4 className="my-0 fw-normal">{service.title}</h4>
+                </div>
+                <div className="card-body">
+                  <h1 className="card-title pricing-card-title">
+                    {service.price}
+                    <small className="text-muted fw-light">/hr</small>
+                  </h1>
+                  <ul className="list-unstyled mt-3 mb-4">
+                    <li>{service.details}</li>
+                  </ul>
+                  <button type="button" className="btn btn-primary">
+                    Book Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
