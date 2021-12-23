6db28f583a10c8d68799996de2f866b06cabc9ec
David Saah <dasorange.hope@gmail.com>
2021-12-23 10:52:19 +0000

    update services page

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import "./Services.css";
+import code from "../../Assets/images/services.jpg";
 
 function Services() {
-    return (
-        <div>
-            <h1>Services Page</h1>
-        </div>
-    )
+  return (
+    <div className="services">
+      <img src={code} alt="Code on the screen" className="services__img" />
+      <div className="services__text">
+        <h1>Services Page</h1>
+        <p className="display-6">List of services will be here.</p>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
