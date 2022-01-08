3ff4a87e488a2a09db1f8a1b7ee0fa97fa1d2062
David Saah <dasorange.hope@gmail.com>
2022-01-08 22:46:10 +0000

    remove nav link for Orders page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Navbar from "./Components/Navbar/Navbar";
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from "./Pages/About/About";
-import Orders from "./Pages/Orders/Orders";
 import Comm from "./Pages/Community/Comm";
 import Services from "./Pages/Services/Services";
 import Footer from "./Components/Footer/Footer";
@@ -21,7 +20,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} exact/>
         <Route path='/about' element={<About />} />
-        <Route path='/orders' element={<Orders />} />
         <Route path='/community' element={<Comm />} />
         <Route path='/services' element={<Services />} />
       </Routes>
