44e502b75cf3414804e2e998906507353e0a776d
David Saah <dasorange.hope@gmail.com>
2022-02-02 23:20:48 +0000

    change homepage layout

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,18 +6,16 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="home">
-      <div className="home__welcome">
-        <img src={homeImg} alt="People reviewing code" className="home__img" />
-        <div className="home__text">
-          <h1>Welcome To CodeReviewShop</h1>
-          <p className="display-6">
-            CodeReviewShop is a place where you can find the best code review
-            services. We have a variety of services to choose from.
-          </p>
-          <Link to="/services">
-            <button type="button" className="btn btn-primary">Book Now</button>
-          </Link>
-        </div>
+      <img src={homeImg} alt="People reviewing code" className="home__img" />
+      <div className="home__text">
+        <h1>Welcome To CodeReviewShop</h1>
+        <p className="display-6">
+          CodeReviewShop is a place where you can find the best code review
+          services. We have a variety of services to choose from.
+        </p>
+        <Link to="/services" className="container">
+          <button className="btn btn-primary">Book Now</button>
+        </Link>
       </div>
     </div>
   );
