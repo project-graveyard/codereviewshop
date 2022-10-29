eb89c935f5eb242cf316f362c8e402661b979671
David Saah <dasorange.hope@gmail.com>
2022-10-29 10:57:02 +0000

    remove about and community page routes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
-import About from "./Pages/About/About";
-import Comm from "./Pages/Community/Comm";
 import Services from "./Pages/Services/Services";
 import Footer from "./Components/Footer/Footer";
 
@@ -19,8 +17,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} exact/>
-        <Route path='/about' element={<About />} />
-        <Route path='/community' element={<Comm />} />
         <Route path='/services' element={<Services />} />
       </Routes>
       <Footer />
