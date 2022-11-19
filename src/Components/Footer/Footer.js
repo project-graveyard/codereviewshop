c00f2d19321e17b694716e5b14a86d738a0a9352
David Saah <dasorange.hope@gmail.com>
2022-11-19 19:19:25 +0000

    update footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,17 +10,16 @@ function Footer() {
       </div>
 
       <div className="footer__info">
-      <div className="footer__contact">
-        <h3>Contact Us</h3>
-        <p><i className="bi bi-telephone-fill"></i> +233 50 4429 219</p>
-        <p><i className="bi bi-envelope-fill"></i> codereviewshop@gmail.com</p>
-      </div>
+        <div className="footer__contact">
+          <h3>Contact Us</h3>
+          <i class="bi bi-whatsapp" style={{ color: "green" }}></i> +233 50 4429 219 <br/>
+          <a href="mailto:codereviewshop@gmail.com" rel="noreferrer" target='_blank'>
+            <i className="bi bi-envelope-fill" style={{ color: "red" }}></i> codereviewshop@gmail.com
+          </a>
+        </div>
         <div className="footer__social">
           <a href="https://www.linkedin.com/in/davesaah/" rel="noreferrer" target='_blank'>
             <i className="bi bi-linkedin" style={{ color: "blue" }}></i>
-          </a>
-          <a href="mailto:codereviewshop@gmail.com" rel="noreferrer" target='_blank'>
-            <i className="bi bi-envelope" style={{ color: "red" }}></i>
           </a>
           <a href="https://github.com/DaveSaah" rel="noreferrer" target='_blank'>
             <i className="bi bi-github" style={{ color: "black" }}></i>
@@ -28,7 +27,7 @@ function Footer() {
           <a href="https://twitter.com/dave_saah" rel="noreferrer" target='_blank'>
             <i className="bi bi-twitter" style={{ color: "DeepSkyBlue" }}></i>
           </a>
-      </div>
+        </div>
       </div>
 
     </div>
