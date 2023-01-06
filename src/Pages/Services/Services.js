0efa0ffeacd972cbb8fe035836f22b6a39aaa5e1
David Saah <dasorange.hope@gmail.com>
2023-01-06 23:55:32 +0000

    update services page

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -5,22 +5,11 @@ const service_list = [
   {
     title: "Code review & feedback",
     price: "Free",
-    currency: "",
   },
-  {
-    title: "One-on-one tutoring",
-    price: "25",
-    currency: "GH₵",
-  },
+
   {
     title: "Code walkthrough & assistance",
-    price: "25",
-    currency: "GH₵",
-  },
-  {
-    title: "IT support & consultation",
-    price: "*",
-    currency: "GH₵",
+    price: "GHS 30",
   },
 ];
 
@@ -33,40 +22,54 @@ function Services() {
         <p className="fs-5 text-muted">Choose a service and a plan that works best for you</p>
       </div>
 
-      <div className="services__data">
-        <div className="row row-cols-2 row-cols-md-4 mb-2 text-center">
-          {service_list.map((service, index) => (
-            <div className="col services__content">
-              <div className="card mb-4 rounded-3 shadow-sm border-primary h-100">
-                <div className="card-header py-3 bg-primary text-white border-primary">
-                  <h4 className="my-0 fw-normal">{service.title}</h4>
-                </div>
-                <div className="card-body">
-                  <h1 className="card-title pricing-card-title h1">
-                    <small className="text-muted fw-dark" style={{ "font-size": "15px" }}>{service.currency}</small>
-                    {service.price}
-                  </h1>
-                  <a href='https://calendly.com/codereviewshop/book' className="btn btn-primary text-white">
-                    Book
-                  </a>
-                </div>
+      <div className="row row-cols-md-2 mb-2 text-center">
+        {service_list.map((service, index) => (
+          <div className="col services__content">
+            <div className="card mb-4 rounded-3 shadow-sm border-primary h-100">
+              <div className="card-header py-3 bg-primary text-white border-primary">
+                <h4 className="my-0 fw-normal">{service.title}</h4>
+              </div>
+              <div className="card-body">
+                <h3 className="card-title pricing-card-title h3">
+                  {service.price}
+                  <small className="text-muted fw-light">/hr</small>
+                </h3>
+                <a href='https://calendly.com/codereviewshop/book' className="btn btn-primary text-white">
+                  Book
+                </a>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
 
       <div className="service__msg alert alert-secondary" role="alert">
         <h4 className="alert-heading">Note:</h4><hr />
         <ul>
-          <li>Payment option: <b>mobile money</b></li>
-          <li>Prices are per session.</li>
-          <li>Each session is a 45-minute online call.</li>
+          <li>Payment options: <b>mobile money</b> or <b>cash</b>.</li>
           <li>Payments should be made before a booked session is due.</li>
-          <li>In the likely case where your problems are not solved, your money will be refunded.</li>
+          <li>In the unlikely case where your problems are not solved, your money will be refunded.</li>
+          <li>For one-on-one tutoring sessions, send us an email: codereviewshop@gmail.com</li>
         </ul>
       </div>
-    </div>
+      {/* <div className="alert alert-secondary" role="alert">
+        <h4 className="alert-heading">Charges Spectrum</h4><hr />
+        <table cellPadding={5}>
+          <tr>
+            <td>Minutes</td><td>Cost</td>
+          </tr>
+          <tr>
+            <td>20</td><td>GHS 10</td>
+          </tr>
+          <tr>
+            <td>30</td><td>GHS 15</td>
+          </tr>
+          <tr>
+            <td>40</td><td>GHS 20</td>
+          </tr>
+        </table>
+      </div> */}
+    </div >
   );
 }
 
