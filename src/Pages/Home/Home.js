d54034eceb0a4ae64a110ea7771e69876ca088bd
David Saah <dasorange.hope@gmail.com>
2023-02-18 23:51:18 +0000

    update homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,17 +6,19 @@ function Home() {
   return (
     <div className="home">
       <div className="home__content">
-      <div className="home__text">
-        <h1>Welcome To CodeReviewShop</h1>
-        <p className="display-6 home__text">
-          CodeReviewShop is a place where you can find the best code review
-          services. We have a variety of services to choose from.
-        </p>
-        <Link to="/services" className="container">
-          <button className="btn btn-primary">Book Now</button>
-        </Link>
+        <div className="home__text">
+          <h1>Welcome To CodeReviewShop</h1>
+          <p className="display-6 home__text">
+            CodeReviewShop is my medium of giving back to the community. My aim
+            is to help students grow with a solid foundation of the concepts and
+            tools we use in the industry. Book a slot and let's solve problems
+            together.
+          </p>
+          <Link to="/services" className="container">
+            <button className="btn btn-primary">Book Now</button>
+          </Link>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
