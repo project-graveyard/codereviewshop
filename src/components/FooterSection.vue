441b8653a3e4cd7b10cdb342312afd1fbcafd7c5
David Saah <dasorange.hope@gmail.com>
2023-05-29 23:11:49 +0000

    add copyright to footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,7 +1,13 @@
-<script></script>
+<script setup>
+import { ref } from 'vue'
+
+const curDate = ref(new Date().getFullYear())
+</script>
 
 <template>
-  <footer></footer>
+  <div class="footer text-center">
+    <span>&copy; 2023 {{ !(2023 === curDate) ? "- " + curDate : "" }} CodeReviewShop</span>
+  </div>
 </template>
 
 <style></style>
