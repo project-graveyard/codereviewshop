2a26463c452a805cbea41c99455d61f02b782407
David Saah <dasorange.hope@gmail.com>
2023-05-31 00:00:40 +0000

    replace bootstrap with tailwind css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,120 +1,65 @@
 <script setup>
 import { RouterLink } from 'vue-router'
-// import { ref } from 'vue'
+import { ref } from 'vue';
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
-// const windowWidth = ref(window.innerWidth)
+const navItems = ref([
+  { name: 'Home', href: '/', current: true },
+  { name: 'Services', href: '/services', current: false },
+  { name: 'About Us', href: '/about', current: false },
+  // { name: 'Blog', href: 'https://codereviewblog.web.app', current: false },
+])
+
+// toggle the state of current page
+function toggleState(item) {
+  navItems.value.forEach(element => {
+    item != element ? element.current = false : element.current = true
+  });
+}
 </script>
 
 <template>
-  <nav class="navbar navbar-dark navbar__bg py-2 text-white sticky-top">
-    <div class="container">
-      <!-- Title -->
-      <div class="d-flex align-items-center">
-        <svg width="75" height="70" class="navbar__logo">
-          <image href="/logo.svg" width="70" height="70" />
-        </svg>
-        <h2 class="display-6">Code Review Shop</h2>
-      </div>
-
-      <!-- Nav links toggler -->
-      <div>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar"
-          aria-controls="offcanvasDarkNavbar">
-          <div class="navbar-toggler-icon"></div>
-        </button>
-        
-        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
-          aria-labelledby="offcanvasDarkNavbarLabel">
-
-          <!-- Offcanvas header -->
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
-              <div class="d-flex align-items-center me-3">
-                <svg width="35" height="30">
-                  <image href="/logo.svg" width="30" height="30" />
-                </svg>
-                Code Review Shop
-              </div>
-            </h5>
-            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
-              aria-label="Close"></button>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button -->
+          <DisclosureButton
+            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex flex-shrink-0 items-center">
+            <img class="block h-8 w-auto lg:hidden" src="/logo.svg" alt="CodeReviewShop" />
+            <img class="hidden h-8 w-auto lg:block" src="/logo.svg" alt="CodeReviewShop" />
           </div>
-
-          <!-- Offcanvas body -->
-          <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item">
-                <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
-              </li>
-              <li class="nav-item">
-                <RouterLink class="nav-link" to="/services">Services</RouterLink>
-              </li>
-              <li class="nav-item">
-                <RouterLink class="nav-link" to="/about">About Us</RouterLink>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://codereviewblog.web.app" target="_blank" rel="noreferrer">Blog</a>
-              </li>
-            </ul>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <RouterLink v-for="item in navItems" @click="toggleState(item)" :key="item.name" :to="item.href"
+                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
+                :aria-current="item.current ? 'page' : undefined">{{ item.name }}</RouterLink>
+              <!-- <a href="https://codereviewblog.web.app"
+                class="['text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
+                target="_blank" rel="noreferrer">Blog</a> -->
+            </div>
           </div>
-
-
         </div>
       </div>
-
-
-      <!-- Full nav links -->
-      <!-- <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-content-lg-start">
-        <div class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small text-center">
-          <RouterLink to="/" class="nav-link text-white"><i
-              class="d-block mx-auto mb-1 bi bi-house-fill navbar__icon"></i>Home</RouterLink>
-          <RouterLink to="/services" class="nav-link text-white">
-            <i class="d-block mx-auto mb-1 bi bi-cart-fill navbar__icon"></i>Services
-          </RouterLink>
-          <RouterLink to="/about" class="nav-link text-white">
-            <i class="d-block mx-auto mb-1 bi bi-info-circle navbar__icon"></i>About Us
-          </RouterLink>
-          <a href="https://codereviewblog.web.app" target="_blank" class="nav-link text-white" rel="noreferrer">
-            <i class="d-block mx-auto mb-1 bi bi-pencil-square navbar__icon"></i>Blog</a>
-        </div>
-      </div> -->
     </div>
-  </nav>
+
+    <DisclosurePanel class="sm:hidden" v-slot="{ close }">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <DisclosureButton v-for="item in navItems" @click="toggleState(item); close(ref)" :key="item.name"
+          :as="RouterLink" :to="item.href"
+          :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
+          :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
-<style scoped>
-.navbar__bg {
-  background-color: #212529 !important;
-}
-
-.navbar__icon {
-  font-size: 1.5rem;
-}
-
-/* Alter navbar when elements overflow to more than one line */
-@media only screen and (max-width: 1000px) {
-
-  html,
-  body {
-    width: auto;
-  }
-
-  /* reduce logo scale on medium screens */
-  .navbar__logo {
-    transform: scale(.8);
-  }
-}
-
-/* remove logo for very small screens */
-@media only screen and (max-width: 390px) {
-
-  html,
-  body {
-    width: auto;
-  }
-
-  .navbar__logo {
-    display: none;
-  }
-}
-</style>
+<style scoped></style>
