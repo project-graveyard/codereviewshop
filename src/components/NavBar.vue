7077288e7171e9bec0aaa7a0120bb7165b757d1f
David Saah <dasorange.hope@gmail.com>
2023-05-31 21:07:09 +0000

    increase the height of the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,7 +22,7 @@ function toggleState(item) {
 <template>
   <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
+      <div class="relative flex h-20 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button -->
           <DisclosureButton
