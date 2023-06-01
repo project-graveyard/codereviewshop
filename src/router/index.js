e06a67111d3dc4b96d90f3b9594a91f35282a0df
David Saah <dasorange.hope@gmail.com>
2023-06-01 12:37:08 +0000

    configure 404 error handling

--- src/router/index.js
+++ src/router/index.js
@@ -5,29 +5,34 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "Home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "About",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/services',
-      name: 'services',
-      component: () => import('../views/ServiceView.vue')
+      path: "/services",
+      name: "Services",
+      component: () => import("../views/ServiceView.vue"),
     },
     {
-      path: '/:pathMatch(.*)*',
-      name: 'pageNotFound',
-      component: () => import('../views/PageNotFoundView.vue')
-    }
-  ]
-})
+      path: "/:catchAll(.*)*",
+      name: "PageNotFound",
+      component: () => import("../views/PageNotFoundView.vue"),
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = "CodeReviewShop ~ " + to.name;
+  next();
+});
 
 export default router
