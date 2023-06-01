65202c54233e120f13e961e0e66b7145c69753ac
David Saah <dasorange.hope@gmail.com>
2023-06-01 12:39:15 +0000

    add firebase to vue app

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import { firebaseApp, analytics } from './firebase'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(firebaseApp)
+app.use(analytics)
 
 app.mount('#app')
