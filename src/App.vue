57672c0f5dae796d29cbbe0b4c3834f92949f838
David Saah <dasorange.hope@gmail.com>
2023-06-02 00:11:08 +0000

    add consent to analytics dialog box

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import Navbar from './components/NavBar.vue'
-import Footer from './components/FooterSection.vue'
+import { RouterView } from "vue-router";
+import Navbar from "./components/NavBar.vue";
+import Footer from "./components/FooterSection.vue";
+import ConsentDialog from "./components/ConsentDialog.vue";
 </script>
 
 <template>
+  <ConsentDialog />
   <header>
     <Navbar />
     <div class="container">
