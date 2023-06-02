3f01647b893def8a6988e5746433e6a39f1c3853
David Saah <dasorange.hope@gmail.com>
2023-06-02 00:12:08 +0000

    update footer

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -27,7 +27,7 @@ function toggleState(item) {
           <DisclosureButton
             class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           >
-            <span class="sr-only">Open main menu</span>
+            <div class="sr-only">Open main menu</div>
             <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
             <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </DisclosureButton>
