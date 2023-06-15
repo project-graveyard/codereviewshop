2a89bd2d46229a7da447572776dd7ab3df0777a0
David Saah <dasorange.hope@gmail.com>
2023-06-15 01:15:56 +0000

    create an array to hold language and tools options with properties

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,45 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+import CardItem from "../components/CardItem.vue";
+
+const langs_and_tools = [
+  {
+    title: "Java",
+    color: "text-red-800",
+    icon: ["fab", "java"],
+  },
+  {
+    title: "Python",
+    color: "text-yellow-600",
+    icon: ["fab", "python"],
+  },
+  {
+    title: "HTML5",
+    color: "text-red-600",
+    icon: ["fab", "html5"],
+  },
+  {
+    title: "CSS3",
+    color: "text-blue-600",
+    icon: ["fab", "css3-alt"],
+  },
+  {
+    title: "Terminal",
+    color: "text-black-600",
+    icon: ["fas", "terminal"],
+  },
+  {
+    title: "Git",
+    color: "text-red-600",
+    icon: ["fas", "code-branch"],
+  },
+  {
+    title: "Github",
+    color: "text-black-600",
+    icon: ["fab", "github"],
+  },
+];
+</script>
 
 <template>
   <h1>This is the Homepage</h1>
