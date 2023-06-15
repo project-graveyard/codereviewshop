5ce1793891b53f3ac70d8b1d3284ccbb31f0bdfd
David Saah <dasorange.hope@gmail.com>
2023-06-15 01:14:05 +0000

    import font awesome library and specific icons

--- src/main.js
+++ src/main.js
@@ -6,6 +6,30 @@ import App from "./App.vue";
 import router from "./router";
 import { analytics } from "./firebase";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faJava } from "@fortawesome/free-brands-svg-icons";
+import { faPython } from "@fortawesome/free-brands-svg-icons";
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
+import { faCss3Alt } from "@fortawesome/free-brands-svg-icons";
+import { faTerminal } from "@fortawesome/free-solid-svg-icons";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+/* add icons to the font-awesome-icon library */
+library.add(faJava);
+library.add(faPython);
+library.add(faHtml5);
+library.add(faCss3Alt);
+library.add(faTerminal);
+library.add(faCodeBranch);
+library.add(faGithub);
+
 const app = createApp(App);
 
 app.use(analytics);
