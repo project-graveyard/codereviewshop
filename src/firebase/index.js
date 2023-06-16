68636faf2c0c390dc3a93cdf153b433a08f5d95f
David Saah <dasorange.hope@gmail.com>
2023-06-16 17:56:31 +0000

    name the initialised firebase app

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyCBTeoHQ2eo2LbhvTLpO2jkQ9YJLGRAATQ",
-  authDomain: "davesaah-tests.firebaseapp.com",
-  projectId: "davesaah-tests",
-  storageBucket: "davesaah-tests.appspot.com",
-  messagingSenderId: "543723979787",
-  appId: "1:543723979787:web:1d45ef0448fea8868b730e",
-  measurementId: "G-7KY6K70SWF",
+const firebaseInit = initializeApp(
+  {
+    apiKey: "AIzaSyCBTeoHQ2eo2LbhvTLpO2jkQ9YJLGRAATQ",
+    authDomain: "davesaah-tests.firebaseapp.com",
+    projectId: "davesaah-tests",
+    storageBucket: "davesaah-tests.appspot.com",
+    messagingSenderId: "543723979787",
+    appId: "1:543723979787:web:1d45ef0448fea8868b730e",
+    measurementId: "G-7KY6K70SWF",
   },
   "firebaseApp"
 );
