dc24c1f93d3626da08883c2b4e38cefe963eee8f
David Saah <dasorange.hope@gmail.com>
2023-06-16 17:54:29 +0000

    convert firebase and analytics app to vue plugin

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -10,7 +10,22 @@ export const firebaseApp = initializeApp({
   messagingSenderId: "543723979787",
   appId: "1:543723979787:web:1d45ef0448fea8868b730e",
   measurementId: "G-7KY6K70SWF",
-});
+  },
+  "firebaseApp"
+);
+
+export const firebaseApp = {
+  install(app, options) {
+    firebaseInit;
+    getApp("firebaseApp");
+  },
+};
+
+export const analytics = {
+  install(app, options) {
+    getAnalytics(firebaseInit);
+  },
+};
 
 export const analytics = () => {
   getAnalytics(firebaseApp)
