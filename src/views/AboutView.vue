d7c469682bd60e88954abc9d350c1fab6a610a42
David Saah <dasorange.hope@gmail.com>
2023-06-20 14:48:13 +0000

    add about banner

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,36 @@
 <script setup></script>
 
+<!--
+  # Outline
+
+  1. Establish a mission statement.
+    state your goal in the industry up front.
+  2. Outline your company story.
+    Every business has a story to tell. Even if you’re running a startup with a brief history, you’ll want to share your company’s progress. Talk about how you got to where you are today on your about page.
+    Pro tip: Isolate the milestones before your company’s founding, and use them to give readers some backstory on your current venture.
+  3. Reveal how you’ve evolved.
+    There’s no shame in admitting how your business strategy — or even your way of thinking — has changed since you began. In fact, these evolutions can improve the story you tell to website visitors.
+    Talk about where you started, how you’ve grown, and the ideals that have helped your organization mature. Use these moments to show people that you're always ready to change and adapt to the needs of your industry.
+  4. State your “aha!” moment.
+    What was a challenge you faced while developing your company? How did this challenge or discovery shape what you are today?
+  5. Explain whom you serve.
+    identify and mention your core customer.
+  6. Explain what you’re offering them.
+    Start by explaining what problem your offering solves. Then, link to a page with more information.
+  7. Cite examples of whom you’ve served.
+  8. Describe your values.
+ -->
+
 <template>
-  <div>
-    <h1>This is the About page</h1>
+  <div
+    class="h-64 w-full text-center flex flex-col text-white justify-center items-center bg-[url(/aboutImg.jpg)] bg-cover bg-center bg-no-repeat"
+  >
+    <h1 class="text-3xl sm:text-5xl font-semibold p-3">
+      The CodeReviewShop Story
+    </h1>
+    <p class="p-3 italic text-md">Better software is built together</p>
+  </div>
+  <div class="py-4 px-2 container">
+    <div><p>At CodeReviewShop, we believe</p></div>
   </div>
 </template>
