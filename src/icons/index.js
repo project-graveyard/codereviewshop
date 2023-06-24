be681d78440722c5cf2e422a3f19f83d72b6422d
David Saah <dasorange.hope@gmail.com>
2023-06-24 18:23:10 +0000

    split fontawesome icons library setup

---
+++ src/icons/index.js
@@ -0,0 +1,44 @@
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import specific icons */
+import { faJava } from "@fortawesome/free-brands-svg-icons";
+import { faPython } from "@fortawesome/free-brands-svg-icons";
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
+import { faCss3Alt } from "@fortawesome/free-brands-svg-icons";
+import { faTerminal } from "@fortawesome/free-solid-svg-icons";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faSeedling } from "@fortawesome/free-solid-svg-icons";
+import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
+import { faHandshakeSimple } from "@fortawesome/free-solid-svg-icons";
+import { faHandHoldingHeart } from "@fortawesome/free-solid-svg-icons";
+import { faPersonHiking } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the font-awesome-icon library */
+library.add(faJava);
+library.add(faPython);
+library.add(faHtml5);
+library.add(faCss3Alt);
+library.add(faTerminal);
+library.add(faCodeBranch);
+library.add(faGithub);
+library.add(faXmark);
+library.add(faBars);
+library.add(faCircleCheck);
+library.add(faEnvelope);
+library.add(faLinkedin);
+library.add(faTwitter);
+library.add(faHeart);
+library.add(faSeedling);
+library.add(faPeopleGroup);
+library.add(faHandshakeSimple);
+library.add(faHandHoldingHeart);
+library.add(faPersonHiking);
