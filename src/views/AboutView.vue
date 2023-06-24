79aaf7bc7f3ccbf8aead19d942fc189570071a0a
David Saah <dasorange.hope@gmail.com>
2023-06-24 17:51:09 +0000

    add msg under value name

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,33 +1,38 @@
 <script setup>
 const values = [
-  { name: "Patience", desc: "", icon: ["fas", "heart"], color: "text-red-600" },
+  {
+    name: "Patience",
+    desc: "The capacity to tolerate delay without becoming annoyed or anxious easily. Things may not always go our way, but our response impacts the future outcome.",
+    icon: ["fas", "heart"],
+    color: "text-red-600",
+  },
   {
     name: "Curiosity",
-    desc: "",
+    desc: "A path of discovery and openness to learn something. Relevance in our industry requires consistent upskilling; those with the desire to learn are those who remain.",
     icon: ["fas", "person-hiking"],
     color: "text-amber-600",
   },
   {
     name: "Growth mindset",
-    desc: "",
+    desc: "Skills are not immutable; they can be improved through consistent practice and discipline. Learning is a life-long journey.",
     icon: ["fas", "seedling"],
     color: "text-green-600",
   },
   {
     name: "Community",
-    desc: "",
+    desc: "If you want to go fast, go alone; if you want to go far, go with people. We encourage active partipation in communities, whether local or global.",
     icon: ["fas", "people-group"],
     color: "text-black",
   },
   {
     name: "Respect",
-    desc: "",
+    desc: "Everyone has value regardless of differences or beliefs. Treat others the way you want to be treated.",
     icon: ["fas", "handshake-simple"],
     color: "text-slate-600",
   },
   {
     name: "Teachableness",
-    desc: "",
+    desc: "Lessons and inspirations can come from unexpected places. Be willing to teach and learn from others.",
     icon: ["fas", "hand-holding-heart"],
     color: "text-indigo-600",
   },
@@ -43,22 +48,22 @@ const values = [
     </h1>
     <p class="p-3 italic text-md">Better software is built together</p>
   </div>
-  <div class="py-6 px-4 container lg:w-1/2">
+  <div class="pt-6 pb-8 px-4 container lg:w-3/4 2xl:w-1/2">
     <div>
       <h1 class="text-4xl font-mono py-2">Present Day</h1>
       <p class="text-lg">
         CodeReviewShop was originally started to handle the increasing requests
         to assist my colleagues and friends in programing related courses.
-        CodeReviewShop is the meduim I chose to use to share what I have learnt
-        with other students. We do this by guiding the thought process of
-        students to identify and solve any issues they may face in their
-        software engineering journey.
+        CodeReviewShop is the medium I chose to share what I have learnt with
+        other students. We do this by guiding the thought process of students to
+        identify and solve any issues they may face in their software
+        engineering journey.
       </p>
     </div>
     <div>
       <h1 class="text-4xl font-mono py-2">Prospective Future</h1>
       <p class="text-lg">
-        We wish ti expand to high schools and organise workshops for students to
+        We plan to expand to high schools and organise workshops for students to
         build their problem solving skills and mentor those who want to pursue a
         career in software.
       </p>
@@ -66,17 +71,17 @@ const values = [
     <div>
       <h1 class="text-4xl font-mono py-2">Values</h1>
       <div
-        class="pt-3 grid grid-cols-1 sm:grid-cols-2 gap-3 justify-start items-start"
+        class="pt-3 grid grid-cols-1 mx-4 sm:grid-cols-2 gap-5 justify-start items-start"
       >
         <div v-for="item in values">
-          <div class="flex text-2xl items-center justify-center">
+          <div class="flex text-2xl items-center">
             <font-awesome-icon
               :icon="item.icon"
               :class="['mr-2 ', item.color]"
             />
             <p>{{ item.name }}</p>
           </div>
-          <p>{{ item.desc }}</p>
+          <p class="text-lg">{{ item.desc }}</p>
         </div>
       </div>
     </div>
