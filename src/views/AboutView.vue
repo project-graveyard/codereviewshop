af0f3fdedda2504b7e44b94c76cd9c138a4b9a0c
David Saah <dasorange.hope@gmail.com>
2023-06-24 15:10:22 +0000

    add the codereviewshop story.
    - Add the codereviewshop story.
    - Make the page mobile responsive.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -43,7 +43,42 @@ const values = [
     </h1>
     <p class="p-3 italic text-md">Better software is built together</p>
   </div>
-  <div class="py-4 px-2 container">
-    <div><p>At CodeReviewShop, we believe</p></div>
+  <div class="py-6 px-4 container lg:w-1/2">
+    <div>
+      <h1 class="text-4xl font-mono py-2">Present Day</h1>
+      <p class="text-lg">
+        CodeReviewShop was originally started to handle the increasing requests
+        to assist my colleagues and friends in programing related courses.
+        CodeReviewShop is the meduim I chose to use to share what I have learnt
+        with other students. We do this by guiding the thought process of
+        students to identify and solve any issues they may face in their
+        software engineering journey.
+      </p>
+    </div>
+    <div>
+      <h1 class="text-4xl font-mono py-2">Prospective Future</h1>
+      <p class="text-lg">
+        We wish ti expand to high schools and organise workshops for students to
+        build their problem solving skills and mentor those who want to pursue a
+        career in software.
+      </p>
+    </div>
+    <div>
+      <h1 class="text-4xl font-mono py-2">Values</h1>
+      <div
+        class="pt-3 grid grid-cols-1 sm:grid-cols-2 gap-3 justify-start items-start"
+      >
+        <div v-for="item in values">
+          <div class="flex text-2xl items-center justify-center">
+            <font-awesome-icon
+              :icon="item.icon"
+              :class="['mr-2 ', item.color]"
+            />
+            <p>{{ item.name }}</p>
+          </div>
+          <p>{{ item.desc }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
