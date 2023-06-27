ee13addf9aa3b82359a3400a0f5f694b32496a23
David Saah <dasorange.hope@gmail.com>
2023-06-27 19:01:48 +0000

    change flex to grid for mobile responsiveness

--- src/views/ServiceView.vue
+++ src/views/ServiceView.vue
@@ -1,12 +1,12 @@
 <script setup></script>
 
 <template>
-  <div class="py-8 container px-4 lg:w-3/4 2xl:w-1/2">
-    <h1 class="text-center text-5xl pb-3">Book a session with us today</h1>
-    <h3 class="text-2xl font-mono text-slate-700">
+  <div class="pt-8 container px-4 lg:w-3/4 2xl:w-1/2">
+    <h1 class="text-center text-5xl mb-3">Book a session with us today</h1>
+    <h1 class="text-3xl text-slate-700 mt-6">
       Find out if Code Review Shop is a fit for you:
-    </h3>
-    <ul class="py-2 ml-8 list-item list-disc leading-9 text-lg">
+    </h1>
+    <ul class="py-2 ml-8 list-disc leading-9 text-lg">
       <li>
         I am learning a new language at school and things are not quite clear.
       </li>
@@ -24,81 +24,79 @@
       <li>I just installed linux. Directions please.</li>
       <li>I want to start a project. Can I get supervision?</li>
     </ul>
+  </div>
 
-    <h2 class="text-center my-3 text-4xl font-mono">Timeline</h2>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
+  <div class="container mt-6">
+    <h1 class="text-center text-3xl mb-3">Timeline</h1>
+    <div
+      class="w-3/4 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3 container"
+    >
       <div>
-        <p
-          class="text-center text-xl bg-blue-600 py-2.5 text-white rounded-t-lg"
-        >
+        <p class="text-center text-xl bg-blue-600 py-4 text-white rounded-t-lg">
           30-minute session
         </p>
-        <div class="border-x border-b rounded-b-lg px-2 py-2 border-blue-500">
-          <ul class="list-item list-disc leading-9 text-xl h-60 ml-7">
+        <div
+          class="flex flex-col items-center border-x border-b rounded-b-lg px-2 py-2 border-blue-500 md:h-72 h-max"
+        >
+          <ul class="list-disc leading-9 text-xl ml-7 grow">
             <li>Project supervision check-in.</li>
             <li>
               General consulting. E.g. Guide on handling challenging
               assignments.
             </li>
           </ul>
-          <div class="mt-auto">
-            <a href="#"
-              ><p
-                class="mx-20 py-3 bg-blue-600 text-center rounded-md font-semibold text-white"
-              >
-                Book
-              </p></a
+          <a href="#"
+            ><p
+              class="py-3 mb-2 mt-3 px-10 bg-blue-600 text-center rounded-md font-semibold text-white"
             >
-          </div>
+              Book
+            </p></a
+          >
         </div>
       </div>
       <div>
-        <p
-          class="text-center text-xl bg-blue-600 py-2.5 text-white rounded-t-lg"
-        >
+        <p class="text-center text-xl bg-blue-600 py-4 text-white rounded-t-lg">
           45-minute session
         </p>
-        <div class="border-x border-b rounded-b-lg px-2 py-2 border-blue-500">
-          <ul class="list-item list-disc leading-9 text-xl h-60 ml-7">
+        <div
+          class="flex flex-col items-center border-x border-b rounded-b-lg px-2 py-2 border-blue-500 md:h-72 h-max"
+        >
+          <ul class="list-disc leading-9 text-xl ml-7 grow">
             <li>Practice problem solving.</li>
             <li>Learn more about tools you already use.</li>
             <li>Setup linux for software development.</li>
           </ul>
-          <div class="mt-auto">
-            <a href="#"
-              ><p
-                class="mx-4 py-3 bg-blue-600 text-center rounded-md font-semibold text-white"
-              >
-                Book
-              </p></a
+          <a href="#"
+            ><p
+              class="py-3 px-10 bg-blue-600 text-center rounded-md font-semibold text-white"
             >
-          </div>
+              Book
+            </p></a
+          >
         </div>
       </div>
       <div>
-        <p
-          class="text-center text-xl bg-blue-600 py-2.5 text-white rounded-t-lg"
-        >
-          1 hour session
+        <p class="text-center text-xl bg-blue-600 py-4 text-white rounded-t-lg">
+          1-hour session
         </p>
-        <div class="border-x border-b rounded-b-lg px-2 py-2 border-blue-500">
-          <ul class="list-item list-disc leading-9 text-xl h-60 ml-7">
+        <div
+          class="flex flex-col items-center border-x border-b rounded-b-lg px-2 py-2 border-blue-500 h-56 lg:h-72"
+        >
+          <ul class="list-disc leading-9 text-xl ml-3 2xl:ml-0 grow">
             <li>I am new to programming.</li>
             <li>I want to learn a new tool.</li>
           </ul>
-          <div class="mt-auto">
-            <a href="#"
-              ><p
-                class="mx-4 py-3 bg-blue-600 text-center rounded-md font-semibold text-white"
-              >
-                Book
-              </p></a
+          <a href="#"
+            ><p
+              class="py-3 px-10 bg-blue-600 text-center rounded-md font-semibold text-white"
             >
-          </div>
+              Book
+            </p></a
+          >
         </div>
       </div>
     </div>
-    <div class="text-lg text-center">
+    <div class="text-lg text-center my-8 mx-3">
       <p>
         * The services listed under each block is a guide. You are free to pick
         a time that suits you.
