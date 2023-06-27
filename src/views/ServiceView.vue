70427f9e67c73385edf240b4fd496fb6707b0c85
David Saah <dasorange.hope@gmail.com>
2023-06-27 20:41:02 +0000

    replace timeline section with a button
    Implement the different timings on the book site

--- src/views/ServiceView.vue
+++ src/views/ServiceView.vue
@@ -27,80 +27,20 @@
   </div>
 
   <div class="container mt-6">
-    <h1 class="text-center text-3xl mb-3">Timeline</h1>
-    <div
-      class="w-3/4 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3 container"
-    >
-      <div>
-        <p class="text-center text-xl bg-blue-600 py-4 text-white rounded-t-lg">
-          30-minute session
-        </p>
-        <div
-          class="flex flex-col items-center border-x border-b rounded-b-lg px-2 py-2 border-blue-500 md:h-72 h-max"
+    <div class="text-center">
+      <a
+        href="https://cal.com/codereviewshop/meet"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button
+          class="bg-blue-600 text-white py-3 px-16 text-lg font-semibold rounded-lg"
         >
-          <ul class="list-disc leading-9 text-xl ml-7 grow">
-            <li>Project supervision check-in.</li>
-            <li>
-              General consulting. E.g. Guide on handling challenging
-              assignments.
-            </li>
-          </ul>
-          <a href="#"
-            ><p
-              class="py-3 mb-2 mt-3 px-10 bg-blue-600 text-center rounded-md font-semibold text-white"
-            >
-              Book
-            </p></a
-          >
-        </div>
-      </div>
-      <div>
-        <p class="text-center text-xl bg-blue-600 py-4 text-white rounded-t-lg">
-          45-minute session
-        </p>
-        <div
-          class="flex flex-col items-center border-x border-b rounded-b-lg px-2 py-2 border-blue-500 md:h-72 h-max"
-        >
-          <ul class="list-disc leading-9 text-xl ml-7 grow">
-            <li>Practice problem solving.</li>
-            <li>Learn more about tools you already use.</li>
-            <li>Setup linux for software development.</li>
-          </ul>
-          <a href="#"
-            ><p
-              class="py-3 px-10 bg-blue-600 text-center rounded-md font-semibold text-white"
-            >
-              Book
-            </p></a
-          >
-        </div>
-      </div>
-      <div>
-        <p class="text-center text-xl bg-blue-600 py-4 text-white rounded-t-lg">
-          1-hour session
-        </p>
-        <div
-          class="flex flex-col items-center border-x border-b rounded-b-lg px-2 py-2 border-blue-500 h-56 lg:h-72"
-        >
-          <ul class="list-disc leading-9 text-xl ml-3 2xl:ml-0 grow">
-            <li>I am new to programming.</li>
-            <li>I want to learn a new tool.</li>
-          </ul>
-          <a href="#"
-            ><p
-              class="py-3 px-10 bg-blue-600 text-center rounded-md font-semibold text-white"
-            >
-              Book
-            </p></a
-          >
-        </div>
-      </div>
+          Book
+        </button>
+      </a>
     </div>
     <div class="text-lg text-center my-8 mx-3">
-      <p>
-        * The services listed under each block is a guide. You are free to pick
-        a time that suits you.
-      </p>
       <p>
         If you like to support our mission, kindly reach out to us via
         <a href="mailto:codereviewshop@gmail.com" class="text-blue-900">email</a
