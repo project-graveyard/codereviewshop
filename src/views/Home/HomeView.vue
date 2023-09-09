74b22e2d27c9aa61070ffa90dcd1b83539a2a60c
David Saah <dasorange.hope@gmail.com>
2023-09-09 20:49:16 +0000

    display upcoming workshop info

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,12 +1,16 @@
 <script setup>
 import FeedbackForm from "./FeedbackForm.vue";
 import WelcomeMsgSection from "./WelcomeMsgSection.vue";
+import SessionInfo from "./SessionInfo.vue";
 </script>
 
 <template>
   <div class="py-8 px-2">
     <!-- Welcome msg section -->
     <WelcomeMsgSection />
+
+    <!--Add past workshops sessions -->
+    <SessionInfo />
 
     <!-- insert feedback form -->
     <FeedbackForm />
