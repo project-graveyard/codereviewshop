76cc3665825a8763ec89bca0ae5be695f4a3c869
David Saah <dasorange.hope@gmail.com>
2023-09-09 23:33:30 +0000

    add Timeline route

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,12 @@ const router = createRouter({
     {
       path: "/about",
       name: "About",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/timeline",
+      name: "Timeline",
+      component: () => import("../views/Timeline/Timeline.vue"),
     },
     {
       path: "/:catchAll(.*)*",
