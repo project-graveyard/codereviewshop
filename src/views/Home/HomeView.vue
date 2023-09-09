d010985545b0491e3df5fe5b476ce33969312e41
David Saah <dasorange.hope@gmail.com>
2023-09-09 00:44:25 +0000

    update homepage

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,16 +1,12 @@
 <script setup>
-import FeedbackForm from "../../components/Home/FeedbackForm.vue";
+import FeedbackForm from "./FeedbackForm.vue";
 import WelcomeMsgSection from "./WelcomeMsgSection.vue";
-import LanguageToolsSection from "./LanguageToolsSection.vue";
 </script>
 
 <template>
   <div class="py-8 px-2">
     <!-- Welcome msg section -->
     <WelcomeMsgSection />
-
-    <!-- Add languages & tools section -->
-    <LanguageToolsSection />
 
     <!-- insert feedback form -->
     <FeedbackForm />
