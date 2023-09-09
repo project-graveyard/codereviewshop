a1c4ee0cdb6252c7c4cefcba124f68d97fcf7a02
David Saah <dasorange.hope@gmail.com>
2023-09-09 23:35:58 +0000

    add timeline page

---
+++ src/views/Timeline/Timeline.vue
@@ -0,0 +1,34 @@
+<script setup>
+/*
+import TimelineItem from './TimelineItem.vue';
+
+const sessions = [
+{
+  org: "Google Developers' Student Club",
+  title: "Git Workshop",
+  date: "Sept 9, 2023",
+  desc: "Touched on the history of git and its relevance. Had hands-on session for students to practice basic git commands such as add, push, pull and commit."
+},
+]
+*/
+</script>
+
+<template>
+  <div class="relative font-inter antialiased mt-6">
+    <div class="text-center text-4xl">Workshop Sessions Timeline</div>
+    <div class="text-center py-3 text-slate-500">Data not available yet...</div>
+    <!-- main class="relative min-h-fit flex flex-col justify-center overflow-hidden">
+      <div class="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
+        <div class="flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-16">
+          <div class="w-full max-w-3xl mx-auto">
+            <div class="-my-24">
+              <div class="relative pl-8 sm:pl-32 py-6 group" v-for="session in sessions">
+                <TimelineItem :org="session.org" :title="session.title" :date="session.date" :desc="session.desc" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main -->
+  </div>
+</template>
